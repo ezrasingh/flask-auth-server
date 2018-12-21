0a907381dd0df64667f2c5c53cce234f0decc65a
Ezra Singh <singhezra@gmail.com>
2018-12-21 15:35:02 -0500

    Basic configuration models for development, testing and production

---
+++ server/config.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import os
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class Development(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
+
+class Testing(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+
+# For use with Heroku
+class Production(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
