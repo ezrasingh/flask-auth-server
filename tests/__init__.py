99a02328d5bcc1d1a9740f8992a2e84c8a9a4e2b
Ezra Singh <singhezra@gmail.com>
2018-12-28 13:58:51 -0500

    Implemented generator function for reusable test instances and contexts

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
 from server import db, models, config, create_app
 
-app = create_app(mode=config.Testing)
+''' Return application test instance and context '''
+def generate_test_env():
+    app = create_app(mode=config.Testing)
+    ctx = app.app_context()
+    return ( app, ctx )
