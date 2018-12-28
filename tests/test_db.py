5c3569a85b847eaf3bffcf7cc36f9417bd363e31
Ezra Singh <singhezra@gmail.com>
2018-12-28 13:59:23 -0500

    Cleaned up unit testing structure and added a user/profile relation test

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,16 +1,34 @@
 #!/usr/bin/env python3
-import unittest
-from tests import app, db
+from unittest import TestCase
+from tests import generate_test_env, db, models
+from tests.utils import populate
 
-class DatabaseTest(unittest.TestCase):
+class DatabaseTest(TestCase):
     def setUp(self):
-        with app.app_context():
-            self.populate()
+        _, self.ctx = generate_test_env()
+        # Initialize context for db testing
+        self.ctx.push()
+        populate(db.session)
     
     def tearDown(self):
-        # NOTE - Testing config uses in-memory SQLite which is volatile by design
-        with app.app_context():
-            db.session.remove()
+        db.session.remove()
+        db.drop_all()
+        # Close testing context
+        self.ctx.pop()
     
-    def populate(self):
-        pass
+    ''' Query sample set from database '''
+    def get_sample_set(self):
+        user = models.User.query.filter_by(email='test@user.com').first()
+        profile = models.Profile.query.filter_by(name='John Doe').first()
+        return ( user, profile )
+
+    ''' Ensure server does not persist plain text password and auth '''
+    def test_user_select(self):
+        user, _ = self.get_sample_set()
+        assert user.password != 'password'
+        assert user.authorize('password')
+    
+    ''' Check for schema enforced one-to-one relationship '''
+    def test_user_profile_relation(self):
+        user, profile = self.get_sample_set()
+        assert user.profile.id == profile.id
