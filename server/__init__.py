b51570448a54f17aad68e6505974d78c36c72cfb
Ezra Singh <singhezra@gmail.com>
2019-01-12 08:27:48 -0500

    Simplified app structure and enabled migrations for future development

--- server/__init__.py
+++ server/__init__.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python3
 from flask import Flask, Blueprint
-from flask_bcrypt import Bcrypt
-from flask_sqlalchemy import SQLAlchemy
-from .config import Development
+from flask_migrate import Migrate
+from server.resources import db, api, security, user_store
 
-db = SQLAlchemy()
-crypto = Bcrypt()
-api = Blueprint('api', __name__, url_prefix='/api')
+migrate = Migrate()
+router = Blueprint('api', 'router', url_prefix='/api')
 
 ''' Generate application instance based on configuration mode '''
 def create_app(mode):
     app = Flask(__name__)
     app.config.from_object(mode)
-    # Initialization
+    migrate.init_app(app, db)
+    api.init_app(router)
+    app.register_blueprint(router)
     with app.app_context():
-        app.register_blueprint(api)
-        crypto.init_app(app)
+        security.init_app(app, user_store)
         db.init_app(app)
         db.create_all()
     return app
