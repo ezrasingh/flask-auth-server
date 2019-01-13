b7612987f35ada4c43c2b3abdbc81d5c689e289a
Ezra Singh <singhezra@gmail.com>
2019-01-13 14:46:17 -0500

    Setup pytest for integration and unit testing

--- tests/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-from server import db, models, config, create_app
-
-''' Return application test instance and context '''
-def generate_test_env():
-    app = create_app(mode=config.Testing)
-    ctx = app.app_context()
-    client = app.test_client()
-    return ( app, ctx, client )
