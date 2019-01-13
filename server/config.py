83760b9241b485f70e103a94c703910a7d3f04d1
Ezra Singh <singhezra@gmail.com>
2019-01-13 14:36:00 -0500

    Config now pulls environment variables via dotenv

--- server/config.py
+++ server/config.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config(object):
     DEBUG = False
     TESTING = False
-    BCRYPT_LOG_ROUNDS = 15
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SECURITY_PASSWORD_HASH = 'bcrypt'
+    SECURITY_PASSWORD_SALT = os.getenv('PASSWORD_SALT')
 
 class Development(Config):
     DEBUG = True
@@ -13,8 +18,11 @@ class Development(Config):
 
 class Testing(Config):
     TESTING = True
+    WTF_CSRF_ENABLED = False
     SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
 
 # For use with Heroku
 class Production(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SECURITY_PASSWORD_SALT = os.getenv('PASSWORD_SALT')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
