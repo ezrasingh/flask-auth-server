3de6ab09b4da7208a1be6ee2261e73a5d3925c70
Ezra Singh <singhezra@gmail.com>
2019-01-13 14:46:52 -0500

    Fixtures for test client and test user

---
+++ tests/conftest.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import pytest
+from server import create_app, config
+from server.resources import db
+
+app = create_app(mode=config.Testing)
+
+@pytest.fixture(scope='module')
+def test_user():
+    return {
+            'email' : 'tester@user.com',
+            'password' : 'letmein',
+            'name' : 'John Doe'
+    }
+
+''' A flask client with a user already authenticated '''
+@pytest.fixture(scope='session')
+def test_client():
+    with app.app_context():
+        db.create_all()
+        yield app.test_client()
+        db.session.remove()
+        db.drop_all()
