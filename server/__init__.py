76391e8dc864e01deb2de41d765e6e6c9eeaf33b
Ezra Singh <singhezra@gmail.com>
2019-02-05 11:10:09 -0500

    Enable CORS for headless frontend development

--- server/__init__.py
+++ server/__init__.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
+import os
 from flask import Flask, Blueprint
 from flask_migrate import Migrate
+from flask_cors import CORS
 from server.resources import db, api, security, user_store
 
 migrate = Migrate()
 router = Blueprint('api', 'router', url_prefix='/api')
+cors = CORS(origin=os.getenv('CLIENT_ORIGIN'), headers=[ 'access-control-allow-origin' ])
 
 ''' Generate application instance based on configuration mode '''
 def create_app(mode):
@@ -12,6 +15,7 @@ def create_app(mode):
     app.config.from_object(mode)
     migrate.init_app(app, db)
     api.init_app(router)
+    cors.init_app(app)
     app.register_blueprint(router)
     with app.app_context():
         security.init_app(app, user_store)
